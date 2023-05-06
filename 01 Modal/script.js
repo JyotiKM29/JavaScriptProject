18be171269cac19fd6c2cec97565f42100f82be7
Jyoti KM <jyotikumari4442@gmail.com>
2023-05-06 12:46:57 +0530

    Completed Work

--- 01 Modal/script.js
+++ 01 Modal/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// console.log(btnsOpenModal);
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const openModal = function()
+{
+
+ console.log('buttonclicked');
+
+ //using class
+ modal.classList.remove('hidden');
+ overlay.classList.remove('hidden');
+
+
+ //using styling
+ // modal.style.display = 'block';
+
+};
+
+for(let i = 0 ; i < btnsOpenModal.length ; i++)
+   btnsOpenModal[i].addEventListener('click',openModal );
+
+btnCloseModal.addEventListener('click', closeModal )
+
+overlay.addEventListener('click',closeModal)

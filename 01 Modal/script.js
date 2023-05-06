661440ba0e744565c0397bfd868373103cc80820
Jyoti KM <jyotikumari4442@gmail.com>
2023-05-06 13:19:22 +0530

    Habdling an 'Esc' Key event

--- 01 Modal/script.js
+++ 01 Modal/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+ 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
@@ -34,3 +34,13 @@ for(let i = 0 ; i < btnsOpenModal.length ; i++)
 btnCloseModal.addEventListener('click', closeModal )
 
 overlay.addEventListener('click',closeModal)
+
+document.addEventListener('keydown', function(e){
+     console.log(e.key);
+
+     if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+       
+            closeModal();
+       
+     }
+})

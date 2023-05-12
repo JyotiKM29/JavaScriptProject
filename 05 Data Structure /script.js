6ce6be985f66b56c9e7489b85af681fa485cf223
Jyoti KM <jyotikumari4442@gmail.com>
2023-05-12 23:38:44 +0530

    Challenge #1 is Solved and added

--- 05 Data Structure /script.js
+++ 05 Data Structure /script.js
@@ -133,3 +133,17 @@ const {team1 , x:draw, team2} = game.odds;
 console.log(team1 ,draw ,team2);
 
 
+// 6.
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+

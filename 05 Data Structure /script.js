2a2b305004627a95f60770720e745988cc96535d
Jyoti KM <jyotikumari4442@gmail.com>
2023-05-14 12:01:47 +0530

    Challenge #4 is Done of Strings

--- 05 Data Structure /script.js
+++ 05 Data Structure /script.js
@@ -301,3 +301,78 @@ for(const [min , event] of gameEvents){
   const half = min <= 45 ? 'FIRST' : 'SECOND' ;
   console.log(`[${half} HALF] ${min} : ${event}`);
 }
+
+
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const text = document.querySelector('textarea').value;
+
+document.querySelector('button').addEventListener('click' ,
+ function(){
+  const text = document.querySelector('textarea').value;
+  const row = text.split('\n');
+  for(const [i ,text] of row.entries()){
+
+    //step 1
+    const newText = text.toLowerCase();
+    const trimText = newText.trim();
+    // console.log(trimText);
+
+    //step 2
+    const index = trimText.indexOf('_');
+    const camelCase = trimText.slice(0 ,index) + trimText[index + 1].toUpperCase() +trimText.slice(index + 2);
+    const findUnderScore = camelCase.replace('_',"");
+
+    // console.log(findUnderScore);
+
+    //step 3
+    const addPadding = findUnderScore.padEnd(20, ' ');
+    // console.log(addPadding);
+
+    //4
+    const ans = addPadding + '✅'.repeat(i +1);
+    console.log(ans);
+    
+
+  }
+
+ 
+// underscoreCase      ✅
+// firstName           ✅✅
+// someVariable        ✅✅✅
+// calculateAge        ✅✅✅✅
+// delayedDeparture    ✅✅✅✅✅
+  // console.log(row);
+ })
